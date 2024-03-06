da5f566ab7120b20288e96b024a81c93c485fa9f
nicolaelefter <nicolaelefter09@gmail.com>
2024-03-06 15:31:14 +0100

    Initial Commit

---
+++ ExersareLinks/ExersareLinksApp.swift
@@ -0,0 +1,32 @@
+//
+//  ExersareLinksApp.swift
+//  ExersareLinks
+//
+//  Created by Nicolae Lefter on 06.03.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ExersareLinksApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
